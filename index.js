da0843000193c3a3cefede0772fe264965f40325
nianyuguai <840311035@qq.com>
2021-02-21 22:36:23 +0800

    add index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const rest = require('request')
+
+
+!(async() => {
+    console.time('sync-jd-script')
+    console.log('====== start sync jd script =====')
+    let res = await main()
+    console.timeEnd('sync-jd-script')
+})()
+
+
+async function main() {
+    let url = `https://jdsharedresourcescdn.azureedge.net/jdresource/jd_redPacket.js`
+    await gotJs(url)
+}
+
+async function gotJs(url) {
+    return new Promise((resovle, reject) => {
+        rest.get({
+            url: url
+        }, (err, response, data) => {
+            console.log('response status code -> ' + response.statusCode)
+            if (err) {
+                console.log('request js err.', err)
+                reject(err)
+            } else {
+                console.log('request js ok')
+                console.log(data)
+                resovle()
+            }
+        })
+    })
+}
